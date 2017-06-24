cd3d020571088a13834699cc99e091ffdf82789e
majid hassani <majid.hassani73@outlook.com>
2017-06-24 15:35:39 +0430

    Initial Commit

---
+++ alma/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  alma
+//
+//  Created by Mehdi Shojaeian on 4/3/1396 AP.
+//  Copyright © 1396 Majid Hassani. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
